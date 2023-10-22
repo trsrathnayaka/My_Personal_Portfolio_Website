7d5988571c6cbefa9f8814b33e71c4721e5d1389
THEVINDU RANSARA RATHNAYAKA <97075043+trsrathnayaka@users.noreply.github.com>
2023-10-22 10:56:42 +0530

    update profile

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,8 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<span>Hi, I am Thevindu Ransara</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit eius officia ea ut vonsectetur adipisicing elit. Illo voluptatum iste, tempora rem obcaecati omnis delectus sunt, consequuntur, et ut quasi aperiam assumenda! <br><br> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex beatae reprehenderit vero maiores dignissimos error vitae, optio inventore explicabo quia officiis magni delectus? Lorem ipsum dolor, sit amet consectetur"
+        "<span>Hi, I am Thevindu Ransara</span> I am Undergraduate Software Engineer in NSBM Green University.I have several skills like Frontend web development and Mobile app development,3D Design Architecture.I have a great attitude towards achieving personal objectives and am an excellent team leader and player. I've worked on a number of projects myself and continue to do so. I have completed multiple 3D projects in the past because I am a 3D designer; the photos from these projects are saved on my Instagram profile. "
+        
       ],
       typeSpeed: 7,
       contentType: 'html'
