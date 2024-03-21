3f646bb19f6c43c0bad0a2102c6b461cdd278605
THEVINDU RANSARA RATHNAYAKA <97075043+trsrathnayaka@users.noreply.github.com>
2024-03-21 10:02:28 +0530

    update new

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Thevindu ", "Feelancer", "Web developer", "Creative Designer"];
+const textArray = ["Thevindu ","Full Stack Web Developer", "UI/UX Designer" , "Freelancer"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
